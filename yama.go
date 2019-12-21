c424e6afc025c7c168c35e5e1b1909c933fbb7d8
Scub-Coral <yamatotarou80004000@gmail.com>
2019-12-21 14:03:19 +0900

    ooo

---
+++ yama.go
@@ -0,0 +1,36 @@
+<<<<<<< HEAD
+package main
+
+import 
+
+
+func main() {
+	ooo.v := Vertex{3, 4}
+	ooo.Scale(v, 10)
+	ooo.fmt.Println(Abs(v))
+}
+=======
+<<<<<<< HEAD
+package main
+
+import 
+
+
+func main() {
+	ooo.v := Vertex{3, 4}
+	ooo.Scale(v, 10)
+	ooo.fmt.Println(Abs(v))
+}
+=======
+package main
+
+import 
+
+
+func main() {
+	ooo.v := Vertex{3, 4}
+	ooo.Scale(v, 10)
+	ooo.fmt.Println(Abs(v))
+}
+>>>>>>> Scub-Coral/master
+>>>>>>> origin/master
