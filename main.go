e00a20f74013336c48a3be2557e1a2844a3788f6
watagori <54665270+watagori@users.noreply.github.com>
2019-12-21 17:51:51 +0900

    echo.テンプレート実装

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+//main関数
+func main() {
+
+	e := echo.New()
+
+	t := &Template{
+		templates: template.Must(template.ParseGlob("html/*.html")),
+	}
+	e.Renderer = t
+	e.GET("/hello", Hello)
+	e.Logger.Fatal(e.Start(":8080"))
+
+}
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+
+}
+
+func Hello(c echo.Context) error {
+	return c.Render(http.StatusOK, "hello", "World")
+}
